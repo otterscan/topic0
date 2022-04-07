d6c207c8b7380ea3495f67449b2934d1c6538d7d
Willian Mitsuda <wmitsuda@gmail.com>
2022-04-07 04:52:01 -0300

    Add performance comment

--- get-sourcify-backup.sh
+++ get-sourcify-backup.sh
@@ -6,6 +6,7 @@ SOURCIFY_IPNS_ROOT="k51qzi5uqu5dll0ocge71eudqnrgnogmbr37gsgl12uubsinphjoknl6bbi4
 echo "Removing previous snapshot..."
 rm -fr sourcify-snapshot && mkdir -p sourcify-snapshot
 
+# note to future maintainers: ipfs get -a (get tar+unpack dir) is faster than non -a ipfs get
 echo "Getting Sourcify repo from ipfs..."
 ipfs get "/ipns/$SOURCIFY_IPNS_ROOT" -o sourcify-snapshot.tar -a
 tar xf sourcify-snapshot.tar -C sourcify-snapshot
